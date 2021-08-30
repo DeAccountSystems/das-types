90c536d547934be6a976dffb15f01bac7e98c5d0
Link <link@blockabc.com>
2021-08-30 11:33:00 +0800

    refactor: reduce the size of ConfigCellSecondaryMarket.sale_description_bytes_limit

--- rust/src/constants.rs
+++ rust/src/constants.rs
@@ -78,6 +78,7 @@ impl TryFrom<u32> for DataType {
             x if x == DataType::ConfigCellProposal as u32 => Ok(DataType::ConfigCellProposal),
             x if x == DataType::ConfigCellProfitRate as u32 => Ok(DataType::ConfigCellProfitRate),
             x if x == DataType::ConfigCellRelease as u32 => Ok(DataType::ConfigCellRelease),
+            x if x == DataType::ConfigCellSecondaryMarket as u32 => Ok(DataType::ConfigCellSecondaryMarket),
             x if x == DataType::ConfigCellRecordKeyNamespace as u32 => Ok(DataType::ConfigCellRecordKeyNamespace),
             x if x == DataType::ConfigCellUnAvailableAccount as u32 => Ok(DataType::ConfigCellUnAvailableAccount),
             x if x == DataType::ConfigCellPreservedAccount00 as u32 => Ok(DataType::ConfigCellPreservedAccount00),
