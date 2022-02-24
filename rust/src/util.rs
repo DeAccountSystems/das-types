8222263f76be519bb02934ad6a38b01faf2a0202
Link <link@blockabc.com>
2022-02-24 12:10:58 +0800

    fix: remove some useless functions in non-std environment

--- rust/src/util.rs
+++ rust/src/util.rs
@@ -1,5 +1,4 @@
 use super::constants::*;
-use super::schemas::packed::*;
 use core::convert::TryFrom;
 use molecule::prelude::*;
 
@@ -11,54 +10,6 @@ pub fn is_entity_eq<T: Entity>(a: &T, b: &T) -> bool {
 
 pub fn is_reader_eq<'a, T: Reader<'a>>(a: T, b: T) -> bool {
     a.as_slice() == b.as_slice()
-}
-
-pub fn wrap_data_entity(version: u32, index: u32, entity: impl Entity) -> DataEntity {
-    DataEntity::new_builder()
-        .version(Uint32::from(version))
-        .index(Uint32::from(index))
-        .entity(Bytes::from(entity.as_slice()))
-        .build()
-}
-
-pub fn wrap_data_entity_opt(version: u32, index: u32, entity: impl Entity) -> DataEntityOpt {
-    DataEntityOpt::new_builder()
-        .set(Some(wrap_data_entity(version, index, entity)))
-        .build()
-}
-
-pub fn wrap_raw_witness(data_type: DataType, mut bytes: Vec<u8>) -> Bytes {
-    let mut data = Vec::new();
-    let mut data_type_bytes = (data_type as u32).to_le_bytes().to_vec();
-    data.append(&mut WITNESS_HEADER.to_vec());
-    data.append(&mut data_type_bytes);
-    data.append(&mut bytes);
-
-    Bytes::new_builder()
-        .set(data.into_iter().map(Byte::new).collect())
-        .build()
-}
-
-pub fn wrap_entity_witness(data_type: DataType, entity: impl Entity) -> Bytes {
-    let mut data = Vec::new();
-    let mut data_type_bytes = (data_type as u32).to_le_bytes().to_vec();
-    data.append(&mut WITNESS_HEADER.to_vec());
-    data.append(&mut data_type_bytes);
-    data.append(&mut entity.as_slice().to_vec());
-
-    Bytes::new_builder()
-        .set(data.into_iter().map(Byte::new).collect())
-        .build()
-}
-
-pub fn wrap_action_witness(action: &str, params_opt: Option<Bytes>) -> Bytes {
-    let mut builder = ActionData::new_builder().action(Bytes::from(action.as_bytes()));
-
-    if let Some(params) = params_opt {
-        builder = builder.params(params);
-    }
-
-    wrap_entity_witness(DataType::ActionData, builder.build())
 }
 
 pub fn data_type_to_char_set(data_type: DataType) -> CharSetType {
