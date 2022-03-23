03ad7722a5751ec34aea4f220e0e1bffb4d820d6
Link <link@blockabc.com>
2022-03-23 17:02:52 +0800

    fix: export some deprecated types

--- rust/src/schemas/mod.rs
+++ rust/src/schemas/mod.rs
@@ -1,9 +1,11 @@
 mod basic;
 mod cell;
+mod cell_v1;
 
 pub mod packed {
     pub use molecule::prelude::{Byte, ByteReader, Reader};
 
     pub use super::basic::*;
     pub use super::cell::*;
+    pub use super::cell_v1::*;
 }
